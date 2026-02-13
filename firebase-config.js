3f81534d416bf7b6a96445464848996c7710fc94
lagneshmitrareturns <lagneshmitrareturns@gmail.com>
2026-02-13 16:39:19 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,25 @@
+// Import Firebase SDKs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
+
+// 🔐 Replace below with YOUR Firebase project config
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Services
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
+
+// Export so main.js can use it
+export { db };
