9bfb886443d4aa8885751edd47bd28bb7f184cdc
lagneshmitrareturns <lagneshmitrareturns@gmail.com>
2026-02-13 18:09:05 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,28 +1,16 @@
-// Import Firebase SDKs
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { getAnalytics, isSupported } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
 
-// 🔐 Replace below with YOUR Firebase project config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "REAL_API_KEY",
+  authDomain: "REAL_PROJECT.firebaseapp.com",
+  projectId: "REAL_PROJECT_ID",
+  storageBucket: "REAL_PROJECT.appspot.com",
+  messagingSenderId: "REAL_SENDER_ID",
+  appId: "REAL_APP_ID"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-// Firestore
-export const db = getFirestore(app);
-
-// Analytics (Safe Initialization)
-isSupported().then((yes) => {
-  if (yes) {
-    getAnalytics(app);
-  }
-});
+export { db };
